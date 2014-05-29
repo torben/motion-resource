ab09682d0b595560c12187d34e06a8c3da22d697
Torben Toepper <tt@htmlcast.de>
2014-05-29 11:48:22 +0200

    Fixes bugs in date parser

--- lib/motion-model-resource/date_parser.rb
+++ lib/motion-model-resource/date_parser.rb
@@ -1,13 +1,18 @@
 module MotionModelResource
   module DateParser
-    def self.parseDate(arg)
-      date_formatter = NSDateFormatter.alloc.init
-      date_formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZ"
+    def self.parse_date(arg)
+      return nil if arg.blank?
+      additional_parsestring = arg.include?(".") ? ".SSS" : ""
+      date_formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss#{additional_parsestring}ZZZZ"
       date = date_formatter.dateFromString arg
 
       return nil if date.blank?
 
-      date.description
+      "#{date.description}"
+    end
+    
+    def self.date_formatter
+      @date_formatter ||= NSDateFormatter.new
     end
   end
 end
